b6c405104cd20e1a8671326092b35179352287d1
Danijel Šugar <danijel.sugar@outlook.com>
2022-01-11 21:19:59 +0100

    Football data api setup

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,143 @@
 
 namespace App\Controller;
 
+use App\Service\FootballDataService;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
      */
-    public function index(): Response
+    public function index(FootballDataService $footballDataService): Response
     {
+        $competitions = $footballDataService->fetchData(
+            'competitions',
+            [
+                'plan' => 'TIER_ONE'
+            ]
+        );
+
+        dump($competitions);
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'competitions' => $competitions,
+        ]);
+    }
+
+    /**
+     * @Route("competitions/{id}", name="app_competitions")
+     */
+    public function competitions(FootballDataService $footballDataService, $id): Response
+    {
+        $competitions = $footballDataService->fetchData(
+            'competitions',
+            [
+                'plan' => 'TIER_ONE'
+            ]
+        );
+
+        $competitionTeams = $footballDataService->fetchData(
+            'competitions/' . $id . '/teams',
+        );
+
+        return $this->render('home/competition.html.twig', [
+            'competitionId' => $id,
+            'competitions' => $competitions,
+            'competitionTeams' => $competitionTeams,
+        ]);
+    }
+
+    /**
+     * @Route("table/{id}", name="app_table")
+     */
+    public function standings(Request $request, FootballDataService $footballDataService, $id): Response
+    {
+        $competitions = $footballDataService->fetchData(
+            'competitions',
+            [
+                'plan' => 'TIER_ONE'
+            ]
+        );
+
+        $standingType = $request->query->get('standingType');
+        if ($standingType) {
+            $competitionStandings = $footballDataService->fetchData(
+                'competitions/' . $id . '/standings',
+                [
+                    'standingType' => $standingType
+                ]
+            );
+        } else {
+            $competitionStandings = $footballDataService->fetchData(
+                'competitions/' . $id . '/standings'
+            );
+        }
+
+        return $this->render('home/table.html.twig', [
+            'competitionId' => $id,
+            'competitions' => $competitions,
+            'competitionStandings' => $competitionStandings
+        ]);
+    }
+
+    /**
+     * @Route("results/{id}", name="app_results")
+     */
+    public function results(FootballDataService $footballDataService, $id): Response
+    {
+        $competitions = $footballDataService->fetchData(
+            'competitions',
+            [
+                'plan' => 'TIER_ONE'
+            ]
+        );
+
+        $competitionResults = $footballDataService->fetchData(
+            'competitions/' . $id . '/matches',
+            [
+                'status' => 'FINISHED'
+            ]
+        );
+
+        dump($competitionResults);
+
+        foreach ($competitionResults->matches as $match) {
+            dump($match->score);
+        }
+
+        return $this->render('home/result.html.twig', [
+            'competitionId' => $id,
+            'competitions' => $competitions,
+            'competitionResults' => $competitionResults
+        ]);
+    }
+     /**
+      * @Route("schedule/{id}", name="app_schedule")
+      */
+    public function schedule(FootballDataService $footballDataService, $id): Response
+    {
+        $competitions = $footballDataService->fetchData(
+            'competitions',
+            [
+                'plan' => 'TIER_ONE'
+            ]
+        );
+
+        $competitionSchedule = $footballDataService->fetchData(
+            'competitions/' . $id . '/matches',
+            [
+                'status' => 'SCHEDULED'
+            ]
+            );
+
+        return $this->render('home/schedule.html.twig', [
+            'competitionId' => $id,
+            'competitions' => $competitions,
+            'competitionSchedule' => $competitionSchedule
         ]);
     }
 
@@ -23,9 +147,7 @@ class HomeController extends AbstractController
      */
     public function admin(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController-admin',
-        ]);
+        return new Response('Pretend admin page');
     }
 
     /**
