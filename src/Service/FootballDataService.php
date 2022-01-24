bda4f170d01a92f094234a399131d3478578078f
Danijel Šugar <danijel.sugar@outlook.com>
2022-01-24 15:06:01 +0100

    Service to retrive data from footballdata api

--- src/Service/FootballDataService.php
+++ src/Service/FootballDataService.php
@@ -4,7 +4,7 @@ namespace App\Service;
 
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
-class FootballDataService
+class FootballDataService implements FootballDataInterface
 {
     private const URL = 'http://api.football-data.org/v2/';
 
@@ -18,7 +18,7 @@ class FootballDataService
         $this->footballApiToken = $footballApiToken;
     }
 
-    public function fetchData(string $uri, $filters = [])
+    public function fetchData(string $uri, array $filters = []): object
     {
         $response = $this->client->request(
             'GET',
@@ -34,5 +34,45 @@ class FootballDataService
         $decoded = json_decode($data);
 
         return $decoded;
+    }
+
+    public function getPredictionRoundsInfo(array $matches): array
+    {
+        $matchdayDates = [];
+        foreach ($matches as $match) {
+            $matchdayDates[] = [
+                'matchday' => $match->matchday,
+                'stage' => $match->stage,
+                'group' => $match->group,
+                'date' => $match->utcDate
+            ];
+        }
+
+        $dates = [];
+        foreach ($matchdayDates as $date) {
+            if (!is_null($date['matchday']) && is_null($date['group']) && $date['stage'] !== 'REGULAR_SEASON') {
+                $dates[$date['stage']][] = $date['date'];
+            } elseif (!is_null($date['matchday'])) {
+                $dates[$date['matchday']][] = $date['date'];
+            } else {
+                $dates[$date['stage']][] = $date['date'];
+            }
+            
+        }
+        
+        $matchdayFirsLastDates = [];
+        foreach ($dates as $matchday => $date) {
+            $firstTimestamp = min(array_map('strtotime', $date));
+            $firstDate = new \DateTime();
+            $firstDate->setTimestamp($firstTimestamp);
+            $matchdayFirsLastDates[$matchday] = $firstDate->format('d.m.y');
+
+            $lastTimestamp = max(array_map('strtotime', $date));
+            $lastDate = new \DateTime();
+            $lastDate->setTimestamp($lastTimestamp);
+            $matchdayFirsLastDates[$matchday] .= ' - ' . $lastDate->format('d.m.Y');
+        }
+
+        return $matchdayFirsLastDates;
     }
 }
