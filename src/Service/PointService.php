7cbdd53e67d26681c6f812955018dfeb32a6271f
Danijel Šugar <danijel.sugar@outlook.com>
2022-02-12 13:34:35 +0100

    Renamed class

---
+++ src/Service/PointService.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Prediction;
+
+class PointService
+{
+    public function calculatePoints(array $match, Prediction $prediction): int
+    {
+        if ($this->correctOutcomeAndScore($match, $prediction)) {
+            return 6;
+        } elseif ($this->correctOutcome($match, $prediction) && ($this->oneTeamScore($match, $prediction) || $this->checkGoalDiff($match, $prediction))) {
+            return 4;
+        } elseif ($this->correctOutcome($match, $prediction) && !$this->oneTeamScore($match, $prediction)) {
+            return 3;
+        }
+        if (!$this->correctOutcome($match, $prediction) && $this->oneTeamScore($match, $prediction)) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    private function correctOutcomeAndScore(array $match, Prediction $prediction)
+    {
+        if ($match['homeTeamScore'] === $prediction->getHomeTeamPrediction() && $match['awayTeamScore'] === $prediction->getAwayTeamPrediction()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function correctOutcome(array $match, Prediction $prediction)
+    {
+        if ($match['homeTeamScore'] > $match['awayTeamScore'] && $prediction->getHomeTeamPrediction() > $prediction->getAwayTeamPrediction() ||
+            $match['homeTeamScore'] < $match['awayTeamScore'] && $prediction->getHomeTeamPrediction() < $prediction->getAwayTeamPrediction() ||
+            $match['homeTeamScore'] === $match['awayTeamScore'] && $prediction->getHomeTeamPrediction() === $prediction->getAwayTeamPrediction()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function oneTeamScore(array $match, Prediction $prediction)
+    {
+        if ($match['homeTeamScore'] === $prediction->getHomeTeamPrediction() || $match['awayTeamScore'] === $prediction->getAwayTeamPrediction()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function checkGoalDiff(array $match, Prediction $prediction)
+    {
+        $matchDiff = $match['homeTeamScore'] - $match['awayTeamScore'];
+        $predictionDiff = $prediction->getHomeTeamPrediction() - $prediction->getAwayTeamPrediction();
+        if ($matchDiff === $predictionDiff) {
+            return true;
+        }
+
+        return false;
+    }
+}
