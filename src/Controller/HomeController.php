ce7b8287cef8723c1c4208748fc829002146febf
Danijel Šugar <danijel.sugar@outlook.com>
2022-04-04 21:28:13 +0200

    Home method returns all prediction for the next 7 days

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\PredictionRepository;
+use App\Repository\RoundMatchRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +14,54 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="app_home")
      */
-    public function home(): Response
-    {
-        return $this->render('home/index.html.twig');
+    public function home(
+        RoundMatchRepository $roundMatchRepository,
+        PredictionRepository $predictionRepository
+    ): Response {
+        $dateTo = (new \DateTime())->modify('+7 day');
+        $matches = $roundMatchRepository->findMatchesForInterval($dateTo);
+
+        /** @var User|null */
+        $user = $this->getUser();
+
+        $sortedMatches = [];
+        foreach ($matches as $match) {
+            $userPrediction = null;
+
+            if ($user) {
+                $userPrediction = $predictionRepository->findPrediction($user, $match);
+            }
+
+            $finished = false;
+
+            $matchDateTime = new \DateTime($match->getDate()->format('Y-m-d H:i'));
+            $currentDateTime = new \DateTime();
+
+            if ($currentDateTime > $matchDateTime) {
+                $finished = true;
+            }
+
+            $sortedMatches[$match->getDate()->format('Y-m-d')][] = [
+                'id' => $match->getMatchId(),
+                'date' => $match->getDate(),
+                'homeTeamName' => $match->getHomeTeamName(),
+                'awayTeamName' => $match->getAwayTeamName(),
+                'fullTimeHomeTeamScore' => $match->getFullTimeHomeTeamScore(),
+                'fullTimeAwayTeamScore' => $match->getFullTimeAwayTeamScore(),
+                'extraTimeHomeTeamScore' => $match->getExtraTimeHomeTeamScore(),
+                'extraTimeAwayTeamScore' => $match->getExtraTimeAwayTeamScore(),
+                'finished' => $finished,
+                'winner' => $match->getWinner(),
+                'competition' => $match->getRound()->getCompetition()->getCompetition(),
+                'competitionName' => $match->getRound()->getCompetition()->getName(),
+                'competitionCode' => $match->getRound()->getCompetition()->getCode(),
+                'competitionEmblem' => $match->getRound()->getCompetition()->getEmblemUrl(),
+                'userPrediction' => $userPrediction,
+            ];
+        }
+
+        return $this->render('home/index.html.twig', [
+            'sortedMatches' => $sortedMatches,
+        ]);
     }
 }
