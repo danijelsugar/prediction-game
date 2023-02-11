91c505a6212e967ae5af031b561674de59821a79
Danijel Šugar <danijel.sugar@outlook.com>
2023-02-11 20:10:42 +0100

    Renamed parameters to be more descriptive. fromFootballData accepts optional parameter competition, new method fromFootballDataSingle is used with v2 API because of API design problems(v4 fixes it by separating match from head2head subresource). Refactored fromFootballDataNew method so it can handle a single match or array of matches

--- src/Factory/MatchFactory.php
+++ src/Factory/MatchFactory.php
@@ -7,12 +7,20 @@ use App\Dto\MatchDto;
 class MatchFactory
 {
     /**
+     * @param object[] $matches
+     *
      * @return MatchDto[]
      */
-    public static function fromFootballData(array $data): array
+    public static function fromFootballData(array $matches, int $competition = null): array
     {
         $dtos = [];
-        foreach ($data as $match) {
+        foreach ($matches as $match) {
+            if (isset($match->competition->id)) {
+                $matchCompetition = $match->competition->id;
+            } else {
+                $matchCompetition = $competition;
+            }
+
             $dtos[] = new MatchDto(
                 $match->id,
                 $match->stage,
@@ -27,20 +35,67 @@ class MatchFactory
                 $match->score->winner,
                 new \DateTimeImmutable($match->lastUpdated),
                 $match->matchday,
-                $match->status
+                $match->status,
+                $matchCompetition,
+                null
             );
         }
 
         return $dtos;
     }
 
-    /**
-     * @return MatchDto[]
-     */
-    public static function fromFootballDataNew(array $data): array
+    public static function fromFootballDataSingle(object $head2Head, object $match): MatchDto
     {
+        return new MatchDto(
+            $match->id,
+            $match->stage,
+            $match->group,
+            new \DateTimeImmutable($match->utcDate),
+            $match->homeTeam->name,
+            $match->awayTeam->name,
+            $match->score->fullTime->homeTeam,
+            $match->score->fullTime->awayTeam,
+            $match->score->extraTime->homeTeam,
+            $match->score->extraTime->awayTeam,
+            $match->score->winner,
+            new \DateTimeImmutable($match->lastUpdated),
+            $match->matchday,
+            $match->status,
+            $match->competition->id,
+            Head2HeadFactory::fromFootballData($head2Head)
+        );
+    }
+
+    /**
+     * @param object[]|object $matches
+     *
+     * @return MatchDto[]|MatchDto
+     */
+    public static function fromFootballDataNew($matches)
+    {
+        if (!is_array($matches)) {
+            return new MatchDto(
+                $matches->id,
+                $matches->stage,
+                $matches->group,
+                new \DateTimeImmutable($matches->utcDate),
+                $matches->homeTeam->name,
+                $matches->awayTeam->name,
+                $matches->score->fullTime->home,
+                $matches->score->fullTime->away,
+                $matches->score->extraTime->home ?? null,
+                $matches->score->extraTime->home ?? null,
+                $matches->score->winner,
+                new \DateTimeImmutable($matches->lastUpdated),
+                $matches->matchday,
+                $matches->status,
+                $matches->competition->id,
+                null
+            );
+        }
+
         $dtos = [];
-        foreach ($data as $match) {
+        foreach ($matches as $match) {
             $dtos[] = new MatchDto(
                 $match->id,
                 $match->stage,
@@ -55,7 +110,9 @@ class MatchFactory
                 $match->score->winner,
                 new \DateTimeImmutable($match->lastUpdated),
                 $match->matchday,
-                $match->status
+                $match->status,
+                $match->competition->id,
+                null
             );
         }
 
