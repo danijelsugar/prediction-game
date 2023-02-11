18395a818cf844033e2f38ea53884722d166106f
Danijel Šugar <danijel.sugar@outlook.com>
2023-02-11 20:30:21 +0100

    removed fetchData method and added separate methods for each API resource/subresource with methods contracts that the class must implement

--- src/Helper/FootballInterface.php
+++ src/Helper/FootballInterface.php
@@ -4,18 +4,58 @@ namespace App\Helper;
 
 use App\Dto\CompetitionDto;
 use App\Dto\MatchDto;
+use App\Dto\StandingDto;
+use App\Dto\TeamDto;
 
 interface FootballInterface
 {
-    public function fetchData(string $uri, array $filters = []);
-
     /**
+     * List all available competitions.
+     *
+     * @param array<string, string> $filters
+     *
      * @return CompetitionDto[]
      */
     public function getCompetitions(array $filters = []): array;
 
     /**
+     * Show Standings for a particular competition.
+     *
+     * @param array<string, string> $filters
+     *
+     * @return StandingDto[]
+     */
+    public function getCompetitionStandings(int $competition, array $filters = []): array;
+
+    /**
+     * List all teams for a particular competition.
+     *
+     * @param array<string, string> $filters
+     *
+     * @return TeamDto[]
+     */
+    public function getCompetitionTeams(int $competition, array $filters = []): array;
+
+    /**
+     * List all matches for a particular competition.
+     *
+     * @param array<string, string> $filters
+     *
      * @return MatchDto[]
      */
-    public function getCompetitionMatches($competition, array $filters = []): array;
+    public function getCompetitionMatches(int $competition, array $filters = []): array;
+
+    /**
+     * Show one particular match.
+     */
+    public function getMatch(int $match): MatchDto;
+
+    /**
+     * List matches across (a set of) competitions.
+     *
+     * @param array<string, string> $filters
+     *
+     * @return MatchDto[]
+     */
+    public function getMatches(array $filters = []): array;
 }
